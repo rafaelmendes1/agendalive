e28ef4592783e9c60f13feafcd4a2920137b63a3
rafaelmendes1 <rarafafa3245@gmail.com>
2020-07-26 19:02:48 -0300

    iniciando a api

---
+++ src/main/java/com/java/workshopmongo/AgendaliveSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.java.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaliveSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaliveSpringbootMongodbApplication.class, args);
+	}
+
+}
